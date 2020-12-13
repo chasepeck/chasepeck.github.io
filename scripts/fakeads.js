6de746bb5e5e0fb9c3d1294d46893f4d26c0c4d3
Chase Peck <52938508+chasepeck@users.noreply.github.com>
2020-12-13 11:04:50 -0500

    5.5.0.b0
    Updated games page, NEW ADS!!

--- scripts/fakeads.js
+++ scripts/fakeads.js
@@ -2,20 +2,16 @@ const adlist={
     "/ads/Execution.png": "/ads/p/execution.html",
     "/ads/Graphic design.png": "/ads/p/graphicdesign.html",
     "/ads/John Code.png": "/ads/p/johncode.html",
-    "/ads/Rich quick.png": "/ads/p/richquick.html",
     "/ads/Secrets.png": "/ads/p/secrets.html",
-    "/ads/Abstract Art.png": "/ads/p/abstractart.html",
     "/ads/Anonymous Classes.png": "/ads/p/anonymousclasses.html",
-    "/ads/Chase Peck for President.png": "/ads/p/president.html",
-    "/ads/Click It.png": "/ads/p/thanks.html",
     "/ads/Glasses for Seniors.png": "/ads/p/glassesforseniors.html",
     "/ads/Kindness Kills.png": "/ads/p/kindnesskills.html",
     "/ads/Never Too Young.png": "/ads/p/president.html",
-    "/ads/Other Candidates.png": "/ads/p/president.html",
     "/ads/Richard Danger Schitzelburgh.png": "/ads/p/dickdanger.html",
-    "/ads/Save Money.png": "/ads/p/savemoney.html",
-    "/ads/X-Tra Long Peanut.png": "/ads/p/peanuts.html",
-    "/ads/Shenzhen Electric Ad.png": "/ads/shenzhenelectric"
+    "/ads/Shenzhen Electric Ad.png": "/ads/shenzhenelectric",
+    "/ads/Hypernet.png": "/ads/p/hypernet.html",
+    "/ads/CPFA.png": "/ads/p/cpfa.html",
+    "/ads/Coolfun.png": "/ads/p/coolfun.html"
 }
 var fakeadslots=document.querySelectorAll(".fakead");
 var i;
