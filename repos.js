d1dc943888c581947efbaf5ef99f7a6341d3255f
Chase Peck <52938508+chasepeck@users.noreply.github.com>
2023-10-22 14:45:39 -0400

    Redesigned front page

--- repos.js
+++ repos.js
@@ -1,3 +1,8 @@
+const leftmin = -20;
+const leftmax = 20;
+const topmin = 0;
+const topmax = 10;
+
 var xhttp = new XMLHttpRequest;
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -5,7 +10,8 @@ xhttp.onreadystatechange = function() {
         var i;
         for(i = 0; i < repodata.length; i++){
             document.getElementById("repos").innerHTML +=
-                "<li><a href=\"" + repodata[i].html_url
+                "<li style='margin-left: " + (Math.random() * (leftmax - leftmin) - leftmin).toString() + "px; margin-top: "
+                + (Math.random() * (topmax - topmin) - topmin).toString() + "px;'><a href=\"" + repodata[i].html_url
                 + "\">" + repodata[i].name
                 + " <i>" + repodata[i].description + "</i></a></li>";
         }
@@ -13,3 +19,8 @@ xhttp.onreadystatechange = function() {
 }
 xhttp.open("GET", "https://api.github.com/users/chasepeck/repos", true);
 xhttp.send();
+
+var elements = document.getElementsByTagName("li");
+for(var i = 0; i < elements.length; i++) {
+    elements[i].style = "margin-left: " + (Math.random() * (leftmax - leftmin) - leftmin).toString() + "px; margin-top: " + (Math.random() * (topmax - topmin) - topmin).toString() + "px;";
+}
