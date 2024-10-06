17a86315564fb1a250d9c96edd007f493f9fdad7
Chase Peck <52938508+chasepeck@users.noreply.github.com>
2024-10-06 08:46:56 -0400

    tmux script

---
+++ tmux.sh
@@ -0,0 +1,14 @@
+session="webdev"
+tmux has-session -d -s $session 2>/dev/null
+
+if [ $? != 0 ]; then
+	tmux new-session -d -s $session -n "editor"
+	tmux send-keys -t $session:editor "vim ." C-m
+
+	tmux new-window -t $session -n "server"
+	tmux send-keys -t $session:server "live-server" C-m
+
+	tmux select-window -t $session:editor
+fi
+
+tmux attach-session -t $session
