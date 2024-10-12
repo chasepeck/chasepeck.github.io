1056c9bed319986c16490895cea33983961a430c
Chase Peck <52938508+chasepeck@users.noreply.github.com>
2024-10-12 13:25:25 -0400

    faxrabbit on sidebar

--- tmux.sh
+++ tmux.sh
@@ -8,6 +8,9 @@ if [ $? != 0 ]; then
 	tmux new-window -t $session -n "server"
 	tmux send-keys -t $session:server "live-server" C-m
 
+	tmux new-window -t $session -n "git"
+	tmux send-keys -t $session:git "lazygit" C-m
+
 	tmux select-window -t $session:editor
 fi
 
